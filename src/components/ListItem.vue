804c33950702dc23aa3ab6d4d5369aaff6e214f8
Connie Cheng <connie.cheng52@gmail.com>
2020-02-23 16:27:29 -0500

    init

---
+++ src/components/ListItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="list-items">
+    <ul>
+      <li v-for="(item, index) in items">{{ index }} - {{ item.message }}</li>
+    </ul>
+    <h3>Essential Links</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  props: {
+    items: Array
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Films.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="films">
+    <h1>This is an film page</h1>
+  </div>
+</template>
