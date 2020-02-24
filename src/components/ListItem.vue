8d066afdebcd7e525e5b4141a773d3a4971d559c
Connie Cheng <connie.cheng52@gmail.com>
2020-02-24 07:08:23 -0500

    add list Item componenet, remove default

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,35 +1,53 @@
 <template>
-  <div class="list-items">
-    <ul>
-      <li v-for="(item, index) in items">{{ index }} - {{ item.message }}</li>
-    </ul>
-    <h3>Essential Links</h3>
-  </div>
+  <v-card id="list-items" class="mx-auto" max-width="350" v-bind:all="all" v-if="all">
+    <v-card-text>
+      <div
+        class="text--primary"
+        v-for="item in itemKeys"
+        :item="item.id"
+        :key="item.id"
+      >{{titleCase(item)}}: {{itemObj[item]}}</div>
+    </v-card-text>
+    <!-- <v-card-actions>
+      <v-btn text color="deep-purple accent-4">More Details</v-btn>
+    </v-card-actions> -->
+  </v-card>
+  <v-card id="list-items" class="mx-auto" min-width="300" v-else>
+    <v-list-item-title class="headline mb-1" v-bind:header="header">{{header}}</v-list-item-title>
+    <v-list-item-subtitle v-bind:supplemental="supplemental">{{supplemental}}</v-list-item-subtitle>
+    <v-expand-transition>
+      <div>
+        <v-divider></v-divider>
+        <v-card-text>
+          <div class="text--primary" v-bind:description="description">{{description}}</div>
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
 </template>
 
 <script>
+import { titleCase } from "../helpers";
 export default {
-  name: "ListItem",
+  name: "list-item",
   props: {
-    items: Array
-  }
+    header: String,
+    description: String,
+    supplemental: String,
+    all: Boolean,
+    itemKeys: Array,
+    itemObj: Object
+  },
+  methods: { titleCase: titleCase },
+  data: () => ({
+    show: false
+  })
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#list-items {
+  margin-bottom: 2%;
+  padding: 1%;
 }
 </style>
